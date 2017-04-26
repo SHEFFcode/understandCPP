5509cadc2352069444068d39f10a80d5221f5045
SHEFFcode <sheff@sheffmachine.com>
2017-04-25 23:16:27 -0700

    declaration

---
+++ understandCPP/DeclarationAndUsage.cpp
@@ -0,0 +1,17 @@
+//
+//  DeclarationAndUsage.cpp
+//  understandCPP
+//
+//  Created by Jeremy Shefer on 4/25/17.
+//  Copyright © 2017 Jeremy Shefer. All rights reserved.
+//
+#include <iostream>
+#include "DeclarationAndUsage.hpp"
+
+void runDeclaration() {
+    int arr[2] = {1, 2}; // declare and intialize the array
+    
+    arr[0] = 2; // you can redefine
+    arr[1] = 4;
+    
+}
---
+++ understandCPP/DeclarationAndUsage.hpp
@@ -0,0 +1,14 @@
+//
+//  DeclarationAndUsage.hpp
+//  understandCPP
+//
+//  Created by Jeremy Shefer on 4/25/17.
+//  Copyright © 2017 Jeremy Shefer. All rights reserved.
+//
+
+#ifndef DeclarationAndUsage_hpp
+#define DeclarationAndUsage_hpp
+
+#include <stdio.h>
+
+#endif /* DeclarationAndUsage_hpp */
