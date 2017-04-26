2c1e82648288d5821a0d717d2cddad5e122eb38f
SHEFFcode <sheff@sheffmachine.com>
2017-04-25 21:49:53 -0700

    another CPP tutorial

---
+++ understandCPP/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  understandCPP
+//
+//  Created by Jeremy Shefer on 4/25/17.
+//  Copyright © 2017 Jeremy Shefer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, world!" << std::endl;
+    
+    return 0; // 0 means success
+    
+}
