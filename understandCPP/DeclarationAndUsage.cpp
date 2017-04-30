11d1f2f5397e2284e744d4995e353ed87e5cd668
SHEFFcode <sheff@sheffmachine.com>
2017-04-29 20:41:23 -0700

    some notes on arrays

--- understandCPP/DeclarationAndUsage.cpp
+++ understandCPP/DeclarationAndUsage.cpp
@@ -11,6 +11,8 @@
 void runDeclaration() {
     int arr[2] = {1, 2}; // declare and intialize the array
     
+    //We have to initialize the array with the same or less number of elements as the array size.
+    
     arr[0] = 2; // you can redefine
     arr[1] = 4;
     
